37ad413f44ad92afe6827f12f647a1e544f451f8
yodahe-Tes <yodaheteshome.kumbi@mail.polimi.it>
2020-03-31 19:22:25 +0200

    included turn manager

---
+++ src/main/java/model/TurnManager.java
@@ -0,0 +1,24 @@
+package model;
+
+/**
+ * A class to manage the game and player turns
+ * @author Kumbi
+ */
+
+public class TurnManager {
+    public TurnManager() {
+    }
+}
+
+
+/**
+ * A class to manage move and phase turns
+ * @author Kumbi
+ */
+
+public class Turn {
+
+    public Turn() {
+    }
+
+}
